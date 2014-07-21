439239154a563a552f70a5d56822be13d8acfec8
Chyld Medford <chyld.medford@gmail.com>
2014-07-21 14:22:17 -0500

    Portfolio#add

--- app/models/portfolio.js
+++ app/models/portfolio.js
@@ -1,8 +1,33 @@
 'use strict';
 
+var Stock = require('./stock');
+
 function Portfolio(name){
   this.name = name;
   this.stocks = [];
+}
+
+Portfolio.prototype.add = function(symbol, amount){
+  var stock = findStock(this.stocks, symbol);
+
+  if(stock){
+    stock.count += amount;
+  }else{
+    stock = new Stock(symbol, amount);
+    this.stocks.push(stock);
+  }
+};
+
+// PRIVATE HELPER FUNCTIONS ///
+
+function findStock(stocks, symbol){
+  for(var i = 0; i < stocks.length; i++){
+    if(stocks[i].symbol === symbol.toUpperCase()){
+      return stocks[i];
+    }
+  }
+
+  return null;
 }
 
 module.exports = Portfolio;
--- test/unit/portfolio.js
+++ test/unit/portfolio.js
@@ -3,6 +3,7 @@
 'use strict';
 
 var expect = require('chai').expect;
+var Stock = require('../../app/models/stock');
 var Portfolio = require('../../app/models/portfolio');
 
 describe('Portfolio', function(){
@@ -13,6 +14,20 @@ describe('Portfolio', function(){
       expect(tech).to.be.instanceof(Portfolio);
       expect(tech.name).to.equal('Tech Portfolio');
       expect(tech.stocks).to.have.length(0);
+    });
+  });
+
+  describe('#add', function(){
+    it('should add a stock object to portfolio stocks', function(){
+      var tech = new Portfolio('Tech Portfolio');
+      tech.add('aapl', 50);
+      tech.add('msft', 35);
+      tech.add('aapl', 25);
+
+      expect(tech.stocks).to.have.length(2);
+      expect(tech.stocks[0]).to.be.instanceof(Stock);
+      expect(tech.stocks[0].count).to.equal(75);
+      expect(tech.stocks[1].count).to.equal(35);
     });
   });
 });
