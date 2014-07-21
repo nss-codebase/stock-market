85338c7fc658535441218c9f1778aa738c9fb408
Chyld Medford <chyld.medford@gmail.com>
2014-07-21 10:56:37 -0500

    Stock constructor

---
+++ test/unit/stock.js
@@ -0,0 +1,20 @@
+/* global describe, it */
+
+'use strict';
+
+var expect = require('chai').expect;
+var Stock = require('../../app/models/stock');
+
+describe('Stock', function(){
+  describe('constructor', function(){
+    it('should create a new stock object', function(){
+      var aapl = new Stock('aApL', '100');
+
+      expect(aapl).to.be.instanceof(Stock);
+      expect(aapl.symbol).to.equal('AAPL');
+      expect(aapl.count).to.equal(100);
+      expect(aapl.price).to.equal(0);
+    });
+  });
+});
+
